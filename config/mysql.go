bad494420f4c06dc0847987059570928e710bdc9
fanchann <fannyarifnass@gmail.com>
2023-09-10 13:05:33 +0700

    add : create docker-compose for easy configuration

--- config/mysql.go
+++ config/mysql.go
@@ -9,11 +9,11 @@ import (
 )
 
 var (
-	dsn = "%s:%s@/%s"
+	dsn = "%s:%s@tcp(%s:%v)/%s?charset=utf8mb4&parseTime=True&loc=Local"
 )
 
 func MysqlConnection(config Config) *sql.DB {
-	db, err := sql.Open(config.Get("db_driver"), fmt.Sprintf(dsn, config.Get("db_username"), config.Get("db_password"), config.Get("db_name")))
+	db, err := sql.Open(config.Get("db_driver"), fmt.Sprintf(dsn, config.Get("db_username"), config.Get("db_password"), config.Get("db_url"), config.Get("db_port"), config.Get("db_name")))
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
